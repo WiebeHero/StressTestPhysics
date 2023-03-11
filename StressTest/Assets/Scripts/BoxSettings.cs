014282caf9f7f25b37cd8c815c7aa02059e17055
WiebeHero <wiebesportel2001@hotmail.com>
2023-03-11 12:09:25 +0100

    Patch 0.02
    - Got all the test results Unity
    - Moving onto Godot 4 testing

--- StressTest/Assets/Scripts/BoxSettings.cs
+++ StressTest/Assets/Scripts/BoxSettings.cs
@@ -1,9 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BoxSettings", menuName = "ScriptableObjects/BoxSettings", order = 0)]
-public class BoxSettings : ScriptableObject
+public class BoxSettings : MonoBehaviour
 {
-    [SerializeField] private bool performMove;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpawnInstances _instances;
+    private string assetPath = Application.streamingAssetsPath;
+    private string[] totalData;
+    private string[] frameData;
+    private int current;
+
+    private void Start()
+    {
+        totalData = new string[2000];
+        frameData = new string[2000];
+        StartCoroutine(StartAnimation());
+        if (!Directory.Exists(assetPath))
+            Directory.CreateDirectory(assetPath);
+        File.Create(assetPath + "/AllData.txt");
+        File.Create(assetPath + "/FrameData.txt");
+    }
+    
+    private void OnDisable()
+    {
+        Debug.Log(totalData.Length);
+        File.WriteAllLines(assetPath + "/AllData.txt", totalData);
+        File.WriteAllLines(assetPath + "/FrameData.txt", frameData);
+    }
+
+    IEnumerator StartAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        List<GameObject> objects = _instances.GetObjects();
+        for (int i = 0; i < objects.Count; i++)
+        {
+            GameObject obj = objects[i].transform.GetChild(0).gameObject;
+            Rigidbody rigid = obj.GetComponent<Rigidbody>();
+            rigid.isKinematic = false;
+            rigid.detectCollisions = true;
+        }
+        _animator.SetTrigger("MoveAndRotate");
+    }
+
+    void Update()
+    {
+        if (current >= totalData.Length)
+            return;
+        totalData[current] = "Frame: " + current + ", Time since last: " + Time.deltaTime;
+        frameData[current] = Time.deltaTime + "";
+        current++;
+    }
+    
 }
--- StressTest/Assets/Scripts/SpawnInstances.cs
+++ StressTest/Assets/Scripts/SpawnInstances.cs
@@ -8,22 +8,30 @@ using UnityEngine;
 public class SpawnInstances : MonoBehaviour
 {
     [SerializeField] private List<SpawnData> data;
+    private List<GameObject> _objects;
     private int _current = 0;
     private int _spawned = 0;
 
     private void Start()
     {
+        _objects = new List<GameObject>();
+        Debug.Log(data.Count);
         for (int i = 0; i < data.Count; i++)
         {
             SpawnData spawnData = data[i];
             for (int x = 0; x < spawnData.SpawnCount; x++)
             {
-                Instantiate(spawnData.Prefab, transform.position, transform.rotation, null);
+                _objects.Add(Instantiate(spawnData.Prefab, transform.position, transform.rotation, null));
             }
         }
     }
     private void Update()
     {
-        Debug.Log("Current FPS: " + 1.0f / Time.deltaTime + ", Current time: " + Time.time);
+        //Debug.Log("Current FPS: " + 1.0f / Time.deltaTime + ", Current time: " + Time.time);
+    }
+
+    public List<GameObject> GetObjects()
+    {
+        return _objects;
     }
 }
