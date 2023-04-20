e4dd0417ef59782049bbc8e72e38a0d41ce6fdf7
WiebeHero <wiebesportel2001@hotmail.com>
2023-04-20 13:27:49 +0200

    Patch 0.07
    + Partial newer data that is now more accurate. Putting it into newer Excels.

--- StressTestGodot/Main.gd
+++ StressTestGodot/Main.gd
@@ -2,12 +2,11 @@ extends Node3D
 
 @onready var spawner = $Spawner
 @onready var animator = $AnimationPlayer
+@export var animation:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	animator.play("Move")
-
+	animator.play(animation)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,5 +17,4 @@ func _notification(what):
 		spawner.save()
 
 func _on_animation_player_animation_finished(anim_name):
-	pass
-	##animator.play(anim_name)
+	animator.play(anim_name)
